249c5fd07deb8916c352a3c9cbeb92899bea6204
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-07-13 19:05:53 +0530

    nat gateway added

--- vpc.tf
+++ vpc.tf
@@ -94,3 +94,21 @@ resource "aws_eip" "nat" {
     }
   )
 }
+## create NAT Gateway
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.nat_gateway_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+
+  )
+ # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
